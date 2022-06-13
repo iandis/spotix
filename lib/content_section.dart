f8d7db296efe6d6e64edfb0515469bc9711768da
Iandi Santulus <iandisantulusn@gmail.com>
2022-06-13 07:21:50 +0700

    chore: remove unused import

--- lib/content_section.dart
+++ lib/content_section.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:spotify_client/spotify_client.dart';
